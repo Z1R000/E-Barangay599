2801f61b15e354a2f9b8a34d6d07e81676f04724
IUsthetic <LEDESMA.FRANCINEVOLTAIRE@UE.EDU.PH>
2021-09-30 22:43:19 +0800

    Create sidebarupdated.php

---
+++ clientms/client/includes/sidebarupdated.php
@@ -0,0 +1,67 @@
+<div class="bg-light" id="sidebar-wrapper">
+            
+            <div class="sidebar-heading text-center py-4 primary-text fs-4 fw-bold text-uppercase border-bottom"><img src = "../images/Barangay.png" style = "width: 60px;"><br>Barangay 599 <br>E-barangay</div>
+
+            <nav class="sidebar py-2 mb-4 bg-light">
+
+                <ul class="nav flex-column" id="nav_accordion">
+                    
+                    <li class="nav-item">
+                        <a href="dashboard.php" class="list-group-item  list-group-item-action bg-transparent second-text active fs-6"><i
+                            class="fas fa-tachometer-alt me-2"></i>Dashboard</a>
+                    </li>
+
+                    <li class="nav-item has-submenu">
+                        <a href="#" class="list-group-item list-group-item-action dropdown-toggle bg-transparent second-text fw-bold nav-link fs-6">
+                            <i class="fas fa-paperclip my-0 me-2"></i>Announcement</a>
+                        <ul class="submenu collapse">
+                            <li><a class="nav-link" href="current-announcement.php">Current Announcement </a></li>
+                            <li><a class="nav-link" href="announcement-list.php">Announcement History </a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item">
+                        <a href="official-list.php" class="list-group-item  list-group-item-action bg-transparent second-text active fs-6"><i
+                            class="fas fa-user-friends"></i> Officials</a>
+                    </li>
+                    <li class="nav-item has-submenu">
+                        <a href="#" class="list-group-item list-group-item-action dropdown-toggle bg-transparent second-text fw-bold nav-link fs-6">
+                            <i class="fas fa-book my-0 me-2"></i>Request</a>
+                        <ul class="submenu collapse">
+                            <li><a class="nav-link" href="#">Certification Request </a></li>
+                            <li><a class="nav-link" href="#">Blotter Request </a></li>
+                            <li><a class="nav-link" href="#">Rental Request</a></li>
+                            <li><a class="nav-link" href="#">Resident Registration</a></li>
+                            <li><a class="nav-link" href="#">Other Service Request </a></li>
+                        </ul>
+                    </li>
+                    <!--<li class="nav-item has-submenu">
+                        <a href="#" class="list-group-item list-group-item-action bg-transparent second-text fw-bold fs-6 dropdown-toggle" ><i
+                            class="fas fa-book me-2"></i>Request</a>
+
+                        <ul class="submenu collapse">
+                            <li><a class="nav-link" href="#">Certification </a></li>
+                            <li><a class="nav-link" href="#">Blotter </a></li>
+                            <li><a class="nav-link" href="#">Rentals</a></li>
+                            <li><a class="nav-link" href="#">Other Services </a></li>
+                            </ul>
+                        
+                            
+                    </li>-->    
+                    <li class="nav-item has-submenu">
+                        <a href="#" class="list-group-item list-group-item-action dropdown-toggle bg-transparent second-text fw-bold nav-link fs-6">
+                            <i class="fas fa-book my-0 me-2"></i>Record</a>
+                        <ul class="submenu collapse">
+                            <li><a class="nav-link" href="#">Certification Record </a></li>
+                            <li><a class="nav-link" href="#">Blotter Record </a></li>
+                            <li><a class="nav-link" href="#">Rental Record</a></li>
+                            <li><a class="nav-link" href="#">Resident Record</a></li>
+                            <li><a class="nav-link" href="#">Other Service Record </a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </nav>
+            <div class="list-group list-group-flush my-3">
+                <a href="../client/index.php" class="list-group-item list-group-item-action bg-transparent text-danger fw-bold fs-6"><i
+                        class="fas fa-power-off me-2"></i>Logout</a>
+            </div>
+        </div>
