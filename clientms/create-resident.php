8e055a7a0e077dad551c4e967f543c987c6409d1
IUsthetic <LEDESMA.FRANCINEVOLTAIRE@UE.EDU.PH>
2021-10-09 21:50:52 +0800

    Update create-resident.php

--- clientms/create-resident.php
+++ clientms/create-resident.php
@@ -87,6 +87,10 @@ if (strlen($_SESSION['clientmsuid']==0)) {
 		display:none;
 		margin:10px;
 	}
+	.divfortable{
+    background-color: white;
+    overflow: auto;
+    }
     </style>
 
 </head>
@@ -102,138 +106,123 @@ if (strlen($_SESSION['clientmsuid']==0)) {
 
 			<!--yung div mismo-->
             <div class="container-fluid px-4" style="margin-top:25px;">
-				<div style="background-color: aliceblue;border-radius: 25px;padding: 25px;">
-                <h2 class="inner-tittle" style="font-weight: 700; Margin: 0% 3%;">Registration Form</h2>
-<div class="graph-form" >
-<div class="form-body" style="margin: 1% 6%;">
-<form method="post"> 
-	<table style="border: none; width: 100%; font-size: 120%;">
-		
-		<tr>
-			<th><span style="color: #000;">Resident Type</span></th>
-			<th><span style="color: #000;">Last Name</span></th>
-			<th><span style="color: #000;">First Name</span></th>
-			<th><span style="color: #000;">Middle Name</span></th>
-		<tr>
-		<tr>
-			<td><select  name="residenttype"  class="form-control select2" required='true' style="padding: 1px; width: 90%; font-size: 90%; color: black;">
-		<option value="">Choose Resident Type</option>
-		<option value="House Owner">House Owner</option>
-		<option value="Care Taker">Care Taker</option>
-	       	<option value="Rental/Boarder">Rental/Boarder</option>
-		<option value="Living W/Relatives">Living W/Relatives</option>
-		
-	</select></td>
-			<td><input type="text" name="lname" placeholder="Last Name" value="" class="form-control" required='true'  style="width: 90%;font-size: 90%; color: black;"></td>
-			<td><input type="text" name="fname" placeholder="First Name" value="" class="form-control" required='true'  style="width: 90%;font-size: 90%; color: black;"></td>
-			<td><input type="text" name="mname" placeholder="Middle Name" value="" class="form-control" required='true'  style="width: 90%;font-size: 90%; color: black;"></td>
-		</tr>
-		<tr>
-			<td><br></td>
-		</tr>
-		<tr>
-			<th><span style="color: #000;">Purok</th>
-			<th><span style="color: #000;">House Unit/Lot No.</th>
-			<th><span style="color: #000;">Street Name</th>
-			<th><span style="color: #000;">Contact Number</th>
-		</tr>
-		<tr>
-			<td><select  name="prk"  class="form-control select2" required='true' style="padding: 1px; width: 90%; font-size: 90%; color: black;">
-		<option value="">Choose Purok #</option>
-		<option value="1">1</option>
-		<option value="2">2</option>
-		<option value="3">3</option>
-		<option value="4">4</option>
-		<option value="5">5</option>
-		<option value="6">6</option>
-		<option value="7">7</option>
-		<option value="8">8</option>
-		<option value="9">9</option>
-		<option value="10">10</option>
-		</select></td>
-			<td><input type="text" name="hu" placeholder="House Unit/Lot No." value="" class="form-control" required='true'  style="width: 90%;font-size: 90%; color: black;"></td>
-			<td><input type="text" name="stn" placeholder="Street Name" value="" class="form-control" required='true'  style="width: 90%;font-size: 90%; color: black;"></td>
-			<td><input type="text" name="contact" value="" placeholder="Contact" class="form-control" maxlength='10' pattern="[0-9]+" style="width: 90%;font-size: 90%; color: black;"></td>
-		</tr>
-		<tr>
-			<td><br></td>
-		</tr>
-		<tr>
-			<th><span style="color: #000;">Birth Place</th>
-			<th><span style="color: #000;">Birth Date</th>
-			<th><span style="color: #000;">Tin Number</th>
-			<th><span style="color: #000;">SSS Number</th>
-		</tr>
-		<tr>
-			
-			<td><input type="text" name="bplace" placeholder="Birth Place" value="" class="form-control" required='true'  style="width: 90%;font-size: 90%; color: black;"></td>
-			<td><input type="date" name="bdate" placeholder="" value="" class="form-control" required='true'  style="width: 90%;font-size: 90%; color: black;"></td>
-			<td><input type="text" name="tin" value="" placeholder="Tin Number" class="form-control" maxlength='10'  style="width: 90%;font-size: 90%; color: black;"></td>
-			<td><input type="text" name="sss" value="" placeholder="SSS Number"  class="form-control" maxlength='10'  style="width: 90%;font-size: 90%; color: black;"></td>
-			<td>
-		</tr>
-		<tr>
-			<td><br></td>
-		</tr>
-		<tr>
-			<th><span style="color: #000;">Civil Status</th>
-			<th><span style="color: #000;">Voter Status</th>
-			<th><span style="color: #000;">Voters Precinct Number</th>
-			<th><span style="color: #000;">Gender</th>
-		</tr>
-		<tr>
-			<td><select  name="cstatus"  class="form-control select2" required='true' style="padding: 1px; width: 90%; font-size: 90%; color: black;">
-		<option value="">Choose Civil Status</option>
-		<option value="Single">Single</option>
-		<option value="Married">Married</option>
-		<option value="Widow">Widow</option>
-		<option value="Separated">Separated</option>
-		
-	</select></td>
-			<td><select  name="voter"  class="form-control select2" required='true' style="padding: 1px; width: 90%; font-size: 90%; color: black;">
-		<option value="">Voter Status</option>
-		<option value="Yes">Yes</option>
-		<option value="No">No</option>
-		
-			</select></td>
-			<td><input type="text" name="vp" value="0" placeholder="Voters Precinct Number" class="form-control" maxlength='10'  style="width: 90%;font-size: 90%; color: black;"></td>
-			<td><select  name="gnd"  class="form-control select2" required='true' style="padding: 1px; width: 90%; font-size: 90%; color: black;">
-				<option value="">Choose Gender</option>
-				<option value="Male">Male</option>
-				<option value="Female">Female</option>
-			</select></td>
-		</tr>
-		<tr>
-			<td><br></td>
-		</tr>
-		<tr>
-			<th><span style="color: #000;">Email</th>
-			<th><span style="color: #000;">Password</th>
-		</tr>
-		<tr>
-			<td><input type="email" name="email" value="" placeholder="Email address" class="form-control" required='true'  style="width: 90%;font-size: 90%; color: black;"></td>
-			<td><input placeholder="Password" type="password" name="password" required="true" id="password" class="form-control"  style="width: 90%;font-size: 90%; color: black;"></td>
-			<td></td>
-			<td><input type="file" style="width: 200px;"></td>
-		</tr>
-		<tr>
-			<td><br></td>
-		</tr>
-		<tr>
-			
-			<td><a href="index.php" name="cancel" id="cancel" style="color: white; background-color: silver; border: 1px; border-radius:25px; width: 50%; padding: 2.5%;" class="btn btn-default"> Cancel</a></form></td>
-			<td></td><td></td>
-			<td><center><button type="submit" class="btn btn-default" name="submit" id="submit" style="color: white; background-color: #021f4e; border: 1px; width: 50%; border-radius:25px;padding: 2.5%;">Add</button> </center></form></td>
-			
-		</tr>
-	</table>
-	
-	 
-</div>
-</div>
-						</div>
-				
-					</div>
+				<div style="background-color: aliceblue;border-radius: 25px;padding: 10px;">
+                <h2 class="inner-title" style="font-weight: 700; text-align:center;Margin: 0% 3%;">E-Barangay 599 Registration Form</h2>
+				<div class="container-fluid px-4">
+               
+            </div>
+            <div class="container-fluid px-4">
+                <div class="row g-3 my-2">
+                    <div class="col-md-3">
+                        <div class="p-1">
+                               <h5>Last Name: </h5>
+							   <input type="text">
+                        </div>
+                    </div>
+                    <div class="col-md-3">
+                        <div class="p-1">
+
+								<h5>First Name: </h5>
+								<input  type="text">
+                        </div>
+                    </div>
+
+
+                    <div class="col-md-3">
+                        <div class="p-1">
+
+								<h5>Middle Name: </h5>
+								<input  type="text">
+                        </div>
+                    </div>
+
+                    <div class="col-md-3">
+                        <div class="p-1">
+
+								<h5>First Name: </h5>
+								<input type="text">
+                        </div>
+                    </div>
+
+                    <div class="col-md-3">
+                        <div class="p-3 bg-white shadow-sm d-flex justify-content-around align-items-center rounded">
+                            <div>
+                                <h3 class="fs-2" tooltip = "Number of pending requests"><?php
+									$sql6="select count(tblresident.voter) as voter
+									 from tblresident where tblresident.voter='Yes'";
+
+									  $query6 = $dbh -> prepare($sql6);
+									  $query6->execute();
+									  $results6=$query6->fetchAll(PDO::FETCH_OBJ);
+									  foreach($results6 as $row6)
+									{
+
+									$voter=$row6->voter;
+									}echo htmlentities($voter);?></h3>
+                                <a class = "link-dark fs-4 card-text" href ="#">Voters</a>
+                            </div>
+                            <i class="fas fa-vote-yea fs-1 primary-text border rounded-full secondary-bg p-3"></i>
+                        </div>
+                    </div>
+
+                    <div class="col-md-3">
+                        <div class="p-3 bg-white shadow-sm d-flex justify-content-around align-items-center rounded">
+                            <div>
+                                <h3 class="fs-2"><?php
+									$sql7 ="SELECT TIMESTAMPDIFF(YEAR, BirthDate, CURDATE()) AS sen7 from tblresident";
+									$query7 = $dbh -> prepare($sql7);
+									$query7->execute();
+									$results7=$query7->fetchAll(PDO::FETCH_OBJ);
+									$cnt7 = 0;
+									foreach($results7 as $row7){
+										$get7 = $row5->sen7;
+										if ($get7 >= 60){
+											$cnt7 += 1;
+										}
+									}
+									echo htmlentities($cnt7);?></h3>
+                                <a class = "link-dark fs-4 card-text" href ="#">Senior Citizens</a>
+                            </div>
+                            <i class="fas fa-blind fs-1 primary-text border rounded-full secondary-bg p-3"></i>
+                        </div>
+                    </div>
+                    <div class="col-md-3">
+                        <div class="p-3 bg-white shadow-sm d-flex justify-content-around align-items-center rounded">
+                            <div>
+                                <h3 class="fs-2"><?php 
+									$sql8 ="SELECT ID from tbladmin ";
+									$query8 = $dbh -> prepare($sql8);
+									$query8->execute();
+									$results8=$query8->fetchAll(PDO::FETCH_OBJ);
+									$tresidents=$query8->rowCount();
+									echo htmlentities($tresidents);?></h3>
+                                <a class = "link-dark fs-4 card-text" href ="#">Officials</a>
+                            </div>
+                            <i class="fas fa-user-shield fs-1 primary-text border rounded-full secondary-bg p-3"></i>
+                        </div>
+                    </div>
+					<div class="col-md-3">
+                        <div class="p-3 bg-white shadow-sm d-flex justify-content-around align-items-center rounded">
+                            <div>
+                                <h3 class="fs-2"><?php 
+									$sql8 ="SELECT ID from tbladmin ";
+									$query8 = $dbh -> prepare($sql8);
+									$query8->execute();
+									$results8=$query8->fetchAll(PDO::FETCH_OBJ);
+									$tresidents=$query8->rowCount();
+									echo htmlentities($tresidents);?></h3>
+                                <a class = "link-dark fs-4 card-text" href ="#">Officials</a>
+                            </div>
+                            <i class="fas fa-user-shield fs-1 primary-text border rounded-full secondary-bg p-3"></i>
+                        </div>
+                    </div>
+                </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+
                 <!-- /#page-content-wrapper -->
     </div>
 
