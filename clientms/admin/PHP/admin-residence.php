468888548ecf34cc8c797a54c92b617a9eb704c1
dylansssss <60581309+dylansssss@users.noreply.github.com>
2021-10-24 04:07:24 +0800

    add

--- clientms/admin/PHP/admin-residence.php
+++ clientms/admin/PHP/admin-residence.php
@@ -222,28 +222,9 @@ if (strlen($_SESSION['clientmsaid']==0)) {
                     </div>
                 </div>
 				<!--END SEARCH -->
-                    <div class="container-fluid px-5 my-3">
-                        <div class="row px-5 g-0 mt-2  ">
-                            <div class="row shadow-lg g-0">
+                 
 
-                            
-                                <div class="btn-group" role="group" aria-label="Basic example">
-                                <button type="button" class="btn btn-primary">Purok 1</button>
-                                <button type="button" class="btn btn-primary">Purok 2</button>
-                                <button type="button" class="btn btn-primary">Purok 3</button>
-                                <button type="button" class="btn btn-primary">Purok 4</button>
-                                <button type="button" class="btn btn-primary">Purok 5</button>
-                                <button type="button" class="btn btn-primary">Purok 6</button>
 
-                                <button type="button" class="btn btn-primary">Purok 7</button>
-                                <button type="button" class="btn btn-primary">Purok 8</button>
-                                <button type="button" class="btn btn-primary">Purok 9</button>
-                                <button type="button" class="btn btn-primary">Purok 10</button>
-                            </div>
-                        </div>     
-                    </div>
-
-                </div>
                
 				<div class="row g-1 px-5">
                     
