6f3005a5d970547edf1761b4c35bff94e7ea9fbe
pawiiieeee <virayvergel10@gmail.com>
2021-10-29 23:44:06 +0800

    ladada

---
+++ clientms/index1.php
@@ -0,0 +1,76 @@
+
+<!DOCTYPE HTML>
+<html lang = "en">
+<head>
+<title>599 E-Barangay System</title>
+<link rel="icon" href="IMAGES/Barangay.png" type="image/icon type">
+<style>
+	body{
+		text-align: center;
+		font-family: bahnschrift light;
+
+		background-image: url(client/images/barangaybackground.png);
+
+		background-size: cover;
+		background-repeat: no-repeat;
+		color: white;
+	}
+	p{
+		font-size: 22px;
+	}
+	.test {
+  width: 35%;
+  white-space: nowrap;
+  margin: 0px auto;
+
+}
+.button{
+			font-family: bahnschrift light;
+			color: black;
+			padding: 22px;
+			font-size: 20px;
+		}
+		
+		.sub{
+		color: white;
+		background-color: #f00;
+		padding: 5px;
+		font-size: 25px;
+		height: 4.5%;
+		width: 160%;
+		text-decoration: none;
+		border-radius: 25px;
+		font-family: bahnschrift light;
+		border-style: none;
+		padding: 8px;
+		}
+</style>
+	</body>
+<br><br><br><br><br><br><br>
+	<img src="images/barangay.png" alt="logo" width="200" height="200" class="image">
+	<h1> BARANGAY 599, ZONE 59, DISTRICT VI <br>
+		OFFICE OF THE SANGGUNIANG BARANGAY <h1>
+	<p> #4745 Peralta St., V. Mapa Sta. Mesa, Manila <br>
+		Tel. #722-9743 </p> <br>
+	<div class="con1"> 
+		<div class='test'>
+		  <div style='float: left;'><a href="admin/PHP/index.php" class="sub" style="
+				padding: 10px 52px;">ADMIN</a>
+		  </div>
+		  <div style='float: right;'><a href="client/index.php" class="sub" style="
+				padding: 10px 32px;">RESIDENT</a>
+		  </div>
+		</div>
+	</div><br><br>
+	<a href="create-resident.php" class="button" style="color: white;"><i>Click here to register</i></a> 
+<div class="footer" style="position: fixed: left:0; bottom:0; text-align: center;">
+		
+		<?php //include_once('includes/footer.php')
+		//asdawdawdawd
+		//ledesmatest
+		//comment
+		//testtt
+		;?>
+	</div>
+</body>
+</html>
