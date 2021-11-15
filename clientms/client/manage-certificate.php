2a06d0525590fbd0448dcf34777fb9139218a13b
dylansssss <60581309+dylansssss@users.noreply.github.com>
2021-11-15 14:03:57 +0800

    asd

--- clientms/client/manage-certificate.php
+++ clientms/client/manage-certificate.php
@@ -156,7 +156,8 @@ if (strlen($_SESSION['clientmsuid'] == 0)) {
 
                 <div class="container-fluid px-4">
                             <div class="row">
-                        <div class="col-12  mx-auto">
+                        <div class="col-11  mx-auto">
+                            <div class="row">
                         <div class="col-xl-12 col-md-12 col-sm-12">
                             <div class="row my-2 ">
                                 <div class="col-md-8">
@@ -223,6 +224,7 @@ if (strlen($_SESSION['clientmsuid'] == 0)) {
                                         } ?>
                                     </tbody>
                                 </table>
+                            </div>
                             </div>
                             </div>
                         </div>
