c204bcdf2e4e349e7f55f1b46e8ffb960cc8a1cc
pawiiieeee <91128320+pawiiieeee@users.noreply.github.com>
2021-11-18 19:18:27 +0800

    Update FORCALENDAR.php

--- clientms/FORCALENDAR.php
+++ clientms/FORCALENDAR.php
@@ -1,4 +1,197 @@
-<label>OLD DATE & TIME: </label>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <link rel="icon" href="admin/images/Barangay.png" type="image/icon type">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
+    <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" type="text/css" />
+    <link rel="stylesheet" href="front.css">
+    <link rel="icon" href="../IMAGES/Barangay.png" type="image/icon type">
+</head>
+<body>
+<div class="">
+    <div class="aHl">
+
+    </div>
+    <div id=":2g8" tabindex="-1">
+
+    </div>
+    <div id=":2fx" class="ii gt" jslog="20277; u014N:xr6bB; 4:W251bGwsbnVsbCxbXV0.">
+        <div id=":2fw" class="a3s aiL msg-3877720715821999831"><u></u>
+<div style="color:#565a5c;background-color:#f2f5f6;margin:0px 0px 0px 0px">
+  <div style="color:#565a5c;font-family:&quot;Helvetica Neue&quot;,&quot;Helvetica&quot;,Helvetica,Arial,sans-serif;font-size:16px;border-collapse:collapse;max-width:800px;margin:0px auto;padding-top:25px;padding-left:25px;padding-right:25px">
+    <div>
+  <div>
+    <div id="m_-3877720715821999831header-wrapper" style="margin-bottom:16px">
+      <div>
+        <a href="http://localhost/github/E-Barangay599/clientms/" style="text-decoration:none;outline:none" target="_blank" data-saferedirecturl="https://www.google.com/url?q=https://www.unionbankph.com/online/&amp;source=gmail&amp;ust=1637318717008000&amp;usg=AOvVaw3nO1dyxrQBs4Fxwx8djcMx">
+          <img style="width:120px;height:100px" src="admin/images/Barangay.png" class="CToWUd">
+        </a>
+      </div>
+    </div>
+  </div>
+</div>
+    <div id="m_-3877720715821999831body-wrapper">
+      <div style="background-color:#ffffff;padding-top:1px">
+        <div class="m_-3877720715821999831content">
+          
+          <div style="margin:0;padding:0">
+            <div style="margin:0;padding:0;margin-top:1em">
+              Hi <b>[Michael Angelo]</b>,
+            </div>
+
+            <div style="margin:0;padding:0;margin-top:1em">
+              Your [Rental] Certificate request has been [approved].
+            </div>
+
+            <div style="margin:0;padding:0;margin-top:1em">
+              
+            </div>
+
+            <div style="margin:0;padding:0;margin-top:1em">
+              <p>Please do not reply to this message. This is a system-generated email sent to <span>[<a href="mailto:michaelangelodino@gmail.com" target="_blank">michaelangelodino@gmail.com</a>]</span>.</p>
+              <p>Thank you !</p>
+            </div>
+          </div>
+          
+        </div>
+      </div>
+    </div>
+    <div>
+  <div>
+  <div id="m_-3877720715821999831footer-wrapper">
+    <div align="center">
+      <table style="padding-top:30px;padding-bottom:30px;padding-left:30px;padding-right:30px;font-size:13px">
+        <tbody>
+        <tr>
+          <td style="text-align:center">
+            <div style="margin-top:20px">
+              <p style="text-decoration:none;outline:none;color:#9b9b9b;text-align:center">This is an electronic information from Barangay 599 Online System</p>
+            </div>
+          </td>
+        </tr>
+        </tbody>
+      </table><div class="yj6qo"></div><div class="adL">
+    </div></div><div class="adL">
+  </div></div><div class="adL">
+
+  </div></div><div class="adL">
+
+</div></div><div class="adL">
+
+  </div></div><div class="adL">
+
+</div></div><div class="adL">
+
+
+</div></div></div><div id=":2gc" class="ii gt" style="display:none"><div id=":2gd" class="a3s aiL "></div></div><div class="hi"></div></div>
+
+
+<!-- <div class="">
+    <div class="aHl">
+
+    </div>
+    <div id=":2g8" tabindex="-1">
+
+    </div>
+    <div id=":2fx" class="ii gt" jslog="20277; u014N:xr6bB; 4:W251bGwsbnVsbCxbXV0.">
+        <div id=":2fw" class="a3s aiL msg-3877720715821999831"><u></u>
+<div style="color:#565a5c;background-color:#f2f5f6;margin:0px 0px 0px 0px">
+  <div style="color:#565a5c;font-family:&quot;Helvetica Neue&quot;,&quot;Helvetica&quot;,Helvetica,Arial,sans-serif;font-size:16px;border-collapse:collapse;max-width:800px;margin:0px auto;padding-top:25px;padding-left:25px;padding-right:25px">
+    <div>
+  <div>
+    <div id="m_-3877720715821999831header-wrapper" style="margin-bottom:16px">
+      <div>
+        <a href="http://localhost/github/E-Barangay599/clientms/" style="text-decoration:none;outline:none" target="_blank" data-saferedirecturl="https://www.google.com/url?q=https://www.unionbankph.com/online/&amp;source=gmail&amp;ust=1637318717008000&amp;usg=AOvVaw3nO1dyxrQBs4Fxwx8djcMx">
+          <img style="width:120px;height:100px" src="admin/images/Barangay.png" class="CToWUd">
+        </a>
+      </div>
+    </div>
+  </div>
+</div>
+    <div id="m_-3877720715821999831body-wrapper">
+      <div style="background-color:#ffffff;padding-top:1px">
+        <div class="m_-3877720715821999831content">
+          
+          <div style="margin:0;padding:0">
+            <div style="margin:0;padding:0;margin-top:1em">
+              Hi <b>[Michael Angelo]</b>,
+            </div>
+
+            <div style="margin:0;padding:0;margin-top:1em">
+              You requested to reset your password.
+            </div>
+
+            <div style="margin:0;padding:0;margin-top:1em">
+              Your user ID is: <b>[USERID]</b>
+            </div>
+
+            <div style="margin:0;padding:0;margin-top:1em">
+              Click on the button below to reset your password.
+            </div>
+
+            <br>
+
+            <div style="margin:0;padding:0;margin-top:1em;text-align:center">
+              <a style="background-image:linear-gradient(to right,#145DA0 0%,#0C2D48 40%,#0C2D48 100%);display:inline-block;min-width:50%;padding:10px 16px;text-decoration:none;border-radius:50px;border:1px solid;text-align:center;vertical-align:middle;white-space:nowrap;border-color:#0C2D48;background-color:#fa8a36;color:#fff" target="_blank" data-saferedirecturl="https://www.google.com/url?q=http://online.unionbankph.com/online-banking/reset/password/4MBxxd34V9lkFeUbUUJWyOSsZqXGHB4MOdfycL5aCUGgxE8jYh&amp;source=gmail&amp;ust=1637318717009000&amp;usg=AOvVaw0twv9y_VgwGNGM4EtDl3Wl">
+                Reset Password <span style="font-size:24px">›</span>
+              </a>
+            </div>
+
+            <br>
+
+            <div style="margin:0;padding:0;margin-top:1em">
+              If you didn’t make this request, simply ignore this message.
+            </div>
+
+            <div style="margin:0;padding:0;margin-top:1em">
+              <p>Please do not reply to this message. This is a system-generated email sent to <span>[<a href="mailto:michaelangelodino@gmail.com" target="_blank">michaelangelodino@gmail.com</a>]</span>.</p>
+              <p>Thank you !</p>
+            </div>
+          </div>
+          
+        </div>
+      </div>
+    </div>
+    <div>
+  <div>
+  <div id="m_-3877720715821999831footer-wrapper">
+    <div align="center">
+      <table style="padding-top:30px;padding-bottom:30px;padding-left:30px;padding-right:30px;font-size:13px">
+        <tbody>
+        <tr>
+          <td style="text-align:center">
+            <div style="margin-top:20px">
+              <p style="text-decoration:none;outline:none;color:#9b9b9b;text-align:center">This is an electronic information from Barangay 599 Online System</p>
+            </div>
+          </td>
+        </tr>
+        </tbody>
+      </table><div class="yj6qo"></div><div class="adL">
+    </div></div><div class="adL">
+  </div></div><div class="adL">
+
+  </div></div><div class="adL">
+
+</div></div><div class="adL">
+
+  </div></div><div class="adL">
+
+</div></div><div class="adL">
+
+
+</div></div></div><div id=":2gc" class="ii gt" style="display:none"><div id=":2gd" class="a3s aiL "></div></div><div class="hi"></div></div> -->
+
+
+
+
+</body>
+
+</html>
+
+<!-- <label>OLD DATE & TIME: </label>
 <input id="startDur" type="datetime-local" name="startDur" />
 <input type="text" id="rate" value="10" disabled>
 <p>
@@ -7,4 +200,4 @@
 <p>
 <label>DIFFERENCE IN HOURS :</label>
 <br>
-<input id="total" type="text" name="total" />
+<input id="total" type="text" name="total" /> -->
